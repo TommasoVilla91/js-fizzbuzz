5c58057868f5e499fc3b3ef85d55e2c9adb4b8f2
TommasoVilla91 <tommasovilla@yahoo.it>
2024-10-29 15:07:56 +0100

    feat: exercise completed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-// 
-
+// DICHIARAZIONE
+// stampo numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
+
+    // individuo divisibili per 3 e 5 
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`FizzBuzz`)
+    
+    // individuo divisibili per 3 
+    } else if (i % 3 === 0) {
         console.log(`Fizz`);
+
+    // individuo divisibili per 5
     } else if (i % 5 === 0) {
         console.log(`Buzz`);
-    } else if () {
-        console.log(`FizzBuzz`)
+
+    // restanti numeri
     } else {
         console.log(i)
     }
